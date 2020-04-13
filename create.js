b733d1ca18040779c254ac23c5ff7d6d3c111bef
Michael Leners <micleners@gmail.com>
2020-04-13 07:02:52 -0500

    Finish create endpoint

--- create.js
+++ create.js
@@ -4,10 +4,20 @@ import AWS from 'aws-sdk';
 const dynamoDb = new AWS.DynamoDB.DocumentClient();
 
 export function main(event, context, callback) {
+  console.log('APPLES');
+  // Request body is passed in as a JSON encoded string in 'event.body'
   const data = JSON.parse(event.body);
 
   const params = {
     TableName: process.env.tableName,
+    // 'Item' contains the attributes of the item to be created
+    // - 'userId': user identities are federated through the
+    //             Cognito Identity Pool, we will use the identity id
+    //             as the user id of the authenticated user
+    // - 'noteId': a unique uuid
+    // - 'content': parsed from request body
+    // - 'attachment': parsed from request body
+    // - 'createdAt': current Unix timestamp
     Item: {
       userId: event.requestContext.identity.cognitoIdentityId,
       noteId: uuid.v1(),
@@ -18,11 +28,13 @@ export function main(event, context, callback) {
   };
 
   dynamoDb.put(params, (error, data) => {
+    // Set response headers to enable CORS (Cross-Origin Resource Sharing)
     const headers = {
       'Access-Control-Allow-Origin': '*',
       'Access-Control-Allow-Credentials': true,
     };
 
+    // Return status code 500 on error
     if (error) {
       const response = {
         statusCode: 500,
@@ -33,6 +45,7 @@ export function main(event, context, callback) {
       return;
     }
 
+    // Return status code 200 and the newly created item
     const response = {
       statusCode: 200,
       headers: headers,
