6e34ff093377c8c737f7062c1088b5c9a1d001ae
Michael Leners <micleners@gmail.com>
2020-04-17 20:35:31 -0500

    CRUD Built

---
+++ list.js
@@ -0,0 +1,24 @@
+import handler from './libs/handler-lib';
+import dynamoDb from './libs/dynamodb-lib';
+
+export const main = handler(async (event, context) => {
+  const params = {
+    TableName: process.env.tableName,
+    // Key:   'KeyConditionExpression' defines the condition for the query
+    // Value: 'userId = :userId' only return items with matching 'userId'
+    //        partition key
+    //
+    // Key:   'ExpressionAttributeValues' defines the value in the condition
+    // Value: ':userId' defines 'userId' to be Identity Pool identity id
+    //        of the authenticated user
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: {
+      ':userId': event.requestContext.identity.cognitoIdentityId,
+    },
+  };
+
+  const result = await dynamoDb.query(params);
+
+  // Return the matching list of items in response body
+  return result.Items;
+});
